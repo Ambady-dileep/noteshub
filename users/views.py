649fe38a1df51f8aacf7bd4c02fa81b0fb0c13ef
Ambady Dileep <ambadydileep.pro@gmail.com>
2025-12-05 21:18:04 +0530

    Added the views and template along with the page rendering

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def homepage(request):
+    return render(request,"users/homepage.html")
+
+def signup_view(request):
+    return render(request, "users/signup.html")
+
+def login_view(request):
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    return HttpResponse("Logging out...")
