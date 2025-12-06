73cfb085e5430f956d7c92eca6c10ddfea26ebb2
Ambady Dileep <ambadydileep.pro@gmail.com>
2025-12-06 15:17:31 +0530

    Updated the folder and file structure into isolated components and file management system etc..... for reuseablity and readability

--- users/views.py
+++ users/views.py
@@ -6,9 +6,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-def homepage(request):
-    return render(request,"users/homepage.html")
-
 def signup_view(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
@@ -23,27 +20,38 @@ def signup_view(request):
 
     return render(request, "users/signup.html",{"form":form})
 
+
 def login_view(request):
+    next_url = request.GET.get("next")  # This gets the page user came from
+
     if request.method == "POST":
         form = LoginForm(request.POST)
 
         if form.is_valid():
             username = form.cleaned_data["username"]
             password = form.cleaned_data["password"]
-            user = authenticate(request, username=username,password=password)
+            user = authenticate(request, username=username, password=password)
 
             if user is not None:
-                login(request,user)
+                login(request, user)
+
+                # Redirect user back to the page they originally wanted
+                if next_url:
+                    return redirect(next_url)
+
                 return redirect("homepage")
+
             else:
-                form.add_error(None,"Invalid username or password")
+                form.add_error(None, "Invalid username or password")
+
     else:
         form = LoginForm()
 
     return render(request, "users/login.html", {"form": form})
 
+
 @login_required(login_url='users:login')
-def home_view(request):
+def homepage(request):
     return render(request, "users/homepage.html")
 
 def logout_view(request):
